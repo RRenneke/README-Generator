a589742a5a5bbaa2caa4672216f20734acbc824e
Robin Renneke <rennekerobin@yahoo.com>
2020-10-04 20:47:13 -0500

    updated js files

--- develop/index.js
+++ develop/index.js
@@ -2,3 +2,67 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 
+
+// Questions for users
+const questions = [
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "What is your Github user name?",
+            name: "github_username",
+        },
+        {
+            type: "input",
+            message: "Describe your project.",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "What steps are required to install your project?",
+            name: "install_steps",
+        },
+        {
+            type: "input",
+            message: "Provide instructions and examples for use.",
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: "List your collaborators, third-party assets, and any tutorials used.",
+            name: "collaborators",
+        },
+        {
+            type: "list",
+            message: "Choose your license.",
+            name: "licenses",
+            choices:['MIT', 'GPL v3', 'AGPL', 'None']
+        },
+
+    ];
+ 
+    // Function to write file
+    function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, (err) => {
+            if(err) {
+                throw err;
+            }
+            console.log('success')
+        });
+    }
+
+    // Function to initialize program
+    function init() {
+        inquirer.prompt(questions).then((answers) => {
+
+            const response = markdown(answers);
+            console.log(answers);
+
+            writeToFile('README.md', response);
+        })
+    }
+
+    init();
--- utils/markdown.js
+++ utils/markdown.js
@@ -1,2 +1,54 @@
-// function to generate markdown for README
-function generateMarkdown(data) 
+let tableOfContents = "";
+if (data.tableOfContents) {
+  tableOfContents =
+    `## Table of Contents
+  
+  * [Title](#Title)
+  * [Description](#Description)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Contributors](#Contributors)
+  * [Testing](#Testing)
+  * [Questions](#Questions)`
+}
+
+const issuesBadge = `[![GitHub issues](https://img.shields.io/github/issues/${data.userName}/${data.Title})](https://github.com/${data.userName}/${data.title}/issues)`;
+
+
+let licenseBadge;
+const profileImage = `![${data.userName}](https://github.com/${data.userName}.png?size=200)`;
+
+if (data.license === 'MIT') {
+  licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+} else if (data.license === 'Apache 2.0') {
+  licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+} else if (data.license === 'gpl') {
+  licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+} else if (data.license === 'Unlicensed') {
+  licenseBadge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+}
+
+return `
+
+# ${data.Title}
+## Description
+${data.Description}
+${tableOfContents}
+## Installation
+${data.Installation}
+## Usage
+${data.Usage}
+## License
+${licenseBadge}
+## Contributors
+${data.Contributors}
+## Testing
+${data.Testing}
+## Questions
+${data.Questions}
+
+`;
+}
+
+module.exports = generateMarkdown;
